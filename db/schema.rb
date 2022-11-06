602608432772866d8a2d3cf591fa5ec64ea6c045
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-11-06 22:52:00 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_172042) do
+ActiveRecord::Schema.define(version: 2022_11_03_055547) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "star_rating"
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
